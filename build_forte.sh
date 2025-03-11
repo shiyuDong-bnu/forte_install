ccf093d7825bf0f2b2430ca6ab99307b9fe57d9f
Dong-Shiyu-NJU <2656304384@qq.com>
2025-03-11 19:17:49 +0800

    add hdf5 path

--- build_forte.sh
+++ build_forte.sh
@@ -1,9 +1,12 @@
-rm -rf CMakeCache.txt CMakeFiles
+source_dir=/home/sydong/work/learnspace/quantum_chemistry/software/forte_install/forte
+rm -rf $source_dir/CMakeCache.txt $source_dir/CMakeFiles
 psi4_plugin=`psi4 --plugin-compile`
-$psi4_plugin -S.  \
+$psi4_plugin -S$source_dir \
+	-B$source_dir \
 	-DCMAKE_C_COMPILER=icx \
 	-DCMAKE_CXX_COMPILER=icpx \
 	-Dambit_DIR=$HOME/install_tutorial/ambit/share/cmake/ambit/ \
+	-DHDF5_ROOT=$HOME/install_tutorial/hdf5/cmake \
 	-DCMAKE_BUILD_TYPE=Debug \
 
-cmake --build  .  -j16
+cmake --build  $source_dir  -j16
